72dc88c33fe941d960f5ad18eef62de125beeb76
Karol <inz.zawisza.karol@gmail.com>
2022-10-11 18:12:42 +0200

    added get methods for money

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <v-app>
-    <v-main>
-      <MainApp /> 
+    <v-main class="blue-grey lighten-5">
+      <MainApp :money="money" /> 
     </v-main>
   </v-app>
 </template>
 
 <script>
 import MainApp from "./components/MainApp.vue";
+import apiClient from "./services/apiClient";
 
 export default {
   name: "App",
-
   components: {
     MainApp,
   },
-
   data: () => ({
-    //
+    money:[],
   }),
+  created(){
+    apiClient.get("").then(({data})=>{this.money = data})
+  }
 };
 </script>
